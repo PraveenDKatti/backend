567224d6c86f780214b8d39cb6ed8cb13af3048c
Praveen Katti <praveendk25@gmail.com>
2026-01-27 18:06:40 +0530

    fix: correct syntax error in getUserTweets response

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -29,7 +29,7 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
     return res
     .status(200)
-    json(new ApiResponse(200, tweets, "tweets fetched successfully")).
+    .json(new ApiResponse(200, tweets, "tweets fetched successfully"))
 
 })
 
