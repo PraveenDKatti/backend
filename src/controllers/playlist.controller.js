261a8d5886c967ce12c69a7cf92b9f227e49a2d4
Praveen Katti <praveendk25@gmail.com>
2026-01-30 01:35:29 +0530

    feat: refactor playlist controller and routes; remove verifyPlaylistOwner middleware and improve error handling

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,22 +3,18 @@ import { Playlist } from "../models/playlist.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
-import { Video } from "../utils"
 
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const { name, description } = req.body
 
     //TODO: create playlist
-
-    if (!(name || description)) {
-        throw new ApiError(404, "name and description required")
-    }
+    if (!(name || !description)) throw new ApiError(401, "all fields required")
 
     const playlist = await Playlist.create({
-        name: name,
-        description: description,
-        owner: req.user,
+        name,
+        description,
+        owner: req.user._id,
         videos: []
     })
 
@@ -29,35 +25,23 @@ const createPlaylist = asyncHandler(async (req, res) => {
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     //TODO: get user playlists
-    const playlist = await Playlist.aggregate([
-        {
-            $match: {
-                owner: req.user._id
-            }
-        },
-        {
-            $lookup: {
-                from: "users",
-                localField: "_id",
-                foreignField: "owner",
-                as: "playlist",
-            }
-        }
-    ])
+    const { userId } = req.params
+    if(!isValidObjectId(userId)) throw new ApiError(400, "Invalid User Id")
 
-    if (!playlist) {
-        throw new ApiError(404, "No playlist found")
-    }
+    const playlists = await Playlist.find({owner: userId})
+    if (!playlists) throw new ApiError(404, "No playlist found") 
 
     return res
         .status(200)
-        .json(new ApiResponse(200, playlist, "playlist fetched successfully"))
+        .json(new ApiResponse(200, playlists, "playlist fetched successfully"))
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     //TODO: get playlist by id
-
-    const playlist = req.playlist
+    const { playlistId } = req.params
+    if(!isValidObjectId(playlistId)) throw new ApiError(400, "Invalid Playlist Id")
+    
+    const playlist = await Playlist.findById(playlistId)
 
     return res
         .status(200)
@@ -68,14 +52,13 @@ const getPlaylistById = asyncHandler(async (req, res) => {
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const { playlistId, videoId } = req.params
 
-    const playlist = req.playlist
+    if(!isValidObjectId(playlistId) && !isValidObjectId(videoId)) throw new ApiError(400, "Invalid playlist or video id") 
 
-    if (!playlist) {
-        throw new ApiError(404, "playlist not found")
-    }
-
-    playlist.videos.addToSet(videoId)
-    await playlist.save()
+    const playlist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        { $addToSet: {video: videoId} },
+        { new: true }
+    )
 
     return res
         .status(200)
@@ -86,16 +69,13 @@ const addVideoToPlaylist = asyncHandler(async (req, res) => {
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const { playlistId, videoId } = req.params
     // TODO: remove video from playlist
-
-    const playlist = req.playlist
-
-    playlist.videos.pull(videoId)
-
-    if (!Playlist) {
-        throw new ApiError(401, "something went wrong")
-    }
-
-    await playlist.save()
+    if(!isValidObjectId(playlistId) && !isValidObjectId(videoId)) throw new ApiError(400, "Invalid playlist or video id") 
+    
+    const playlist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        { $pull: { video: videoId } },
+        { new: true }
+    )
 
     return res
         .status(200)
@@ -107,7 +87,8 @@ const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
 const deletePlaylist = asyncHandler(async (req, res) => {
     const { playlistId } = req.params
     // TODO: delete playlist
-    await Playlist.deleteOne(playlistId)
+    if(!isValidObjectId(playlistId)) throw new ApiError(400, "Invalid playlist Id")
+    await Playlist.findByIdAndDelete(playlistId)
 
     return res
         .status(200)
@@ -116,20 +97,15 @@ const deletePlaylist = asyncHandler(async (req, res) => {
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const { playlistId } = req.params
-    const { name = "", description = "" } = req.body
+    const { name, description } = req.body
     //TODO: update playlist
+    if(!isValidObjectId(playlistId)) throw new ApiError(400, "Invalid playlist Id")
 
-    const playlist = req.playlist
+    const playlist = await Playlist.findById(playlistId)
+    if(!playlist) throw new ApiError(404, "Playlist Not Found")
 
-    if (name) {
-        playlist.name = name
-    }
-
-    playlist.description = description
-    if (description) {
-        playlist.description = description
-    }
-
+    if (name) { playlist.name = name }
+    if (description) { playlist.description = description }
     await playlist.save()
 
     return res
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,18 +1,25 @@
-import { addVideoToPlaylist, createPlaylist, deletePlaylist, getPlaylistById, getUserPlaylists, removeVideoFromPlaylist, updatePlaylist } from "../controllers/playlist.controller.js";
+import { Router } from "express";
+import { 
+    addVideoToPlaylist, 
+    createPlaylist, 
+    deletePlaylist, 
+    getPlaylistById, 
+    getUserPlaylists, 
+    removeVideoFromPlaylist, 
+    updatePlaylist 
+} from "../controllers/playlist.controller.js";
 import verifyJWT from "../middlewares/auth.middleware.js"
-import verifyPlaylistOwner from "../middlewares/verifyPlaylistOwner.middleware.js"
-
-const { Router } = require("express");
 
 
 const router = Router()
+router.use(verifyJWT)
 
-router.route("/create").post(verifyJWT, createPlaylist)
-router.route("/getAll").get(verifyJWT, getUserPlaylists)
-router.route("/:playlistId").get(verifyJWT, getPlaylistById)
-router.route("/:playlistId/add").patch(verifyJWT, verifyPlaylistOwner, addVideoToPlaylist)
-router.route("/:playlistId/remove").patch(verifyJWT, verifyPlaylistOwner, removeVideoFromPlaylist)
-router.route("/:playlist/delete").delete(verifyJWT, verifyPlaylistOwner, deletePlaylist)
-router.route("/:playlist/update").patch(verifyJWT, verifyPlaylistOwner, updatePlaylist)
+router.route("/create").post(createPlaylist)
+router.route("/user/:userId").get(getUserPlaylists)
+router.route("/:playlistId").get(getPlaylistById)
+router.route("/add/:playlistId/").patch(addVideoToPlaylist)
+router.route("/remove/:playlistId/").patch(removeVideoFromPlaylist)
+router.route("/:playlistId").delete(deletePlaylist)
+router.route("/update/:playlist").patch(updatePlaylist)
 
 export default router
