87d4a1d4951f08c3608555be60e41729aa8df3b7
Praveen Katti <praveendk25@gmail.com>
2026-01-31 22:55:09 +0530

    feat: remove toggleTweetLike route from like.routes.js

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -3,7 +3,6 @@ import { verifyJWT } from "../middlewares/auth.middleware.js"
 import { 
     getLikedVideos, 
     toggleCommentLike, 
-    toggleTweetLike, 
     toggleVideoLike 
 } from "../controllers/like.controller.js";
 
@@ -12,7 +11,6 @@ router.use(verifyJWT)
 
 router.route("/toggle/v/:videoId").post(toggleVideoLike)
 router.route("/toggle/c/:commentId").post(toggleCommentLike)
-router.route("/toggle/t/:tweetId").post(toggleTweetLike)
 router.route("/videos/").get(getLikedVideos)
 
 export default router
